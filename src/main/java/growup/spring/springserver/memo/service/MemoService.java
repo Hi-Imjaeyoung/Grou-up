609ba280f9428ece72839b47119a7a57afcf9bdc
Hi-Imjaeyoung <imjy2757@naver.com>
2025-03-12 15:10:49 +0900

    feat : MemoRepository service 서비스 테스트 코드 작성.

---
+++ src/main/java/growup/spring/springserver/memo/service/MemoService.java
@@ -0,0 +1,43 @@
+package growup.spring.springserver.memo.service;
+
+import growup.spring.springserver.campaign.domain.Campaign;
+import growup.spring.springserver.exception.memo.MemoNotFoundException;
+import growup.spring.springserver.memo.dto.MemoRequestDto;
+import growup.spring.springserver.memo.MemoTypeChanger;
+import growup.spring.springserver.memo.dto.MemoUpdateRequestDto;
+import growup.spring.springserver.memo.domain.Memo;
+import growup.spring.springserver.memo.repository.MemoRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MemoService {
+    @Autowired
+    private MemoRepository memoRepository;
+
+    public Memo makeMemo(Campaign campaign, MemoRequestDto memoRequestDto){
+        Memo memo = MemoTypeChanger.requestToEntity(memoRequestDto,campaign);
+        return memoRepository.save(memo);
+    }
+
+    public List<Memo> getMemoAboutCampaign(Campaign campaign){
+        return memoRepository.findAllByCampaign(campaign);
+    }
+
+    @Transactional
+    public Memo updateMemo(MemoUpdateRequestDto memoRequestDto){
+        Memo memo = memoRepository.findById(memoRequestDto.getMemoId()).orElseThrow(MemoNotFoundException::new
+        );
+        memo.updateContents(memoRequestDto.getContents());
+        return memo;
+    }
+
+    @Transactional
+    public int deleteMemo(Long id){
+        return memoRepository.deleteMemoById(id);
+    }
+
+}

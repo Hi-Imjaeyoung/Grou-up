019eecffbc04a17aa9da7f7c59ec131867e8a8dd
Hi-Imjaeyoung <imjy2757@naver.com>
2025-03-12 15:10:38 +0900

    feat : MemoRepository Layer 서비스 테스트 코드 작성.

---
+++ src/main/java/growup/spring/springserver/memo/repository/MemoRepository.java
@@ -0,0 +1,19 @@
+package growup.spring.springserver.memo.repository;
+
+import growup.spring.springserver.campaign.domain.Campaign;
+import growup.spring.springserver.memo.domain.Memo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MemoRepository extends JpaRepository<Memo,Long> {
+    List<Memo> findAllByCampaign(Campaign campaign);
+    @Modifying
+    @Query("DELETE FROM Memo m WHERE m.id = :id")
+    int deleteMemoById(@Param("id") Long id);
+}

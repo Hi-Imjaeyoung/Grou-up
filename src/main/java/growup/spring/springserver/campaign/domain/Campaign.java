219e0a63cf7d81a59a80703e54222e3fbe26b4d5
Hi-Imjaeyoung <imjy2757@naver.com>
2025-03-29 11:54:05 +0900

    refactor : 캠패인 entity FK OneToMany 설정 및 Builder default 설정.

--- src/main/java/growup/spring/springserver/campaign/domain/Campaign.java
+++ src/main/java/growup/spring/springserver/campaign/domain/Campaign.java
@@ -1,11 +1,22 @@
 package growup.spring.springserver.campaign.domain;
 
+import growup.spring.springserver.exclusionKeyword.domain.ExclusionKeyword;
+import growup.spring.springserver.execution.domain.Execution;
+import growup.spring.springserver.keyword.domain.Keyword;
+import growup.spring.springserver.keywordBid.domain.KeywordBid;
+import growup.spring.springserver.log.domain.Log;
 import growup.spring.springserver.login.domain.Member;
+import growup.spring.springserver.margin.domain.Margin;
+import growup.spring.springserver.marginforcampaign.domain.MarginForCampaign;
+import growup.spring.springserver.memo.domain.Memo;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import lombok.*;
 import jakarta.persistence.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EntityListeners(AuditingEntityListener.class)
 @Entity
@@ -23,6 +34,31 @@ public class Campaign {
     private String camAdType;
 
     private Boolean camOpen = false;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "campaign", cascade = CascadeType.REMOVE,fetch = FetchType.LAZY)
+    private List<Keyword> keywordList = new ArrayList<>();
+    @Builder.Default
+    @OneToMany(mappedBy = "campaign", cascade = CascadeType.REMOVE,fetch = FetchType.LAZY)
+    private List<ExclusionKeyword> exclusionKeywords = new ArrayList<>();
+    @Builder.Default
+    @OneToMany(mappedBy = "campaign", cascade = CascadeType.REMOVE,fetch = FetchType.LAZY)
+    private List<KeywordBid> keywordBids = new ArrayList<>();
+    @Builder.Default
+    @OneToMany(mappedBy = "campaign", cascade = CascadeType.REMOVE,fetch = FetchType.LAZY)
+    private List<Memo> memos = new ArrayList<>();
+    @Builder.Default
+    @OneToMany(mappedBy = "campaign", cascade = CascadeType.REMOVE,fetch = FetchType.LAZY)
+    private List<MarginForCampaign> marginForCampaigns = new ArrayList<>();
+    @Builder.Default
+    @OneToMany(mappedBy = "campaign", cascade = CascadeType.REMOVE,fetch = FetchType.LAZY)
+    private List<Execution> executions= new ArrayList<>();
+    @Builder.Default
+    @OneToMany(mappedBy = "campaign", cascade = CascadeType.REMOVE,fetch = FetchType.LAZY)
+    private List<Log> logs = new ArrayList<>();
+    @Builder.Default
+    @OneToMany(mappedBy = "campaign", cascade = CascadeType.REMOVE,fetch = FetchType.LAZY)
+    private List<Margin> margins= new ArrayList<>();
 
     @ManyToOne
     @JoinColumn(name = "email", referencedColumnName = "email")

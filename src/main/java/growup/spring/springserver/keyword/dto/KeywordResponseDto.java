69bf11dc01e05e256f2c95cb4de5d0ab9b921d0b
Hi-Imjaeyoung <imjy2757@naver.com>
2025-07-30 20:34:47 +0900

    feat : coupangExcelData Base Entity 구현 후 같은 필드를 값는 엔티티(키워드 및 키워드 Dto)에 상속 처리, 빌드 디폴트 추가.
     - 퍼센트 관련 필드 계산 한 곳에 위임.
     - 이에 따른 변수 이름 변경.
     - 추후 데이터 베이스 컬럼 이름 변경 필수!

--- src/main/java/growup/spring/springserver/keyword/dto/KeywordResponseDto.java
+++ src/main/java/growup/spring/springserver/keyword/dto/KeywordResponseDto.java
@@ -1,41 +1,21 @@
 package growup.spring.springserver.keyword.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import growup.spring.springserver.keyword.domain.Keyword;
-import lombok.Builder;
-import lombok.Data;
+import growup.spring.springserver.global.domain.CoupangExcelData;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
-import java.time.LocalDate;
 import java.util.Map;
 
-@Data
-@Builder
+@SuperBuilder
+@Getter
+@Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class KeywordResponseDto {
+public class KeywordResponseDto extends CoupangExcelData {
 
     private String keyKeyword;  // 키워드
 
-    private Long keyImpressions;  // 노출수
-
-    private Long keyClicks;  // 클릭수
-
-    private Double keyClickRate;  // 클릭률
-
-    private Long keyTotalSales;  // 총 주문수
-
-    private Double keyCvr ;  // 전환율
-
-    private Double keyCpc;  // CPC
-
-    private Double keyAdcost;  // 광고비
-
-    private Double keyAdsales;  // 광고매출
-
-    private Double keyRoas ;  // ROAS
-
-    private String keyDate;  // 날짜
-
-    private Boolean keyExcludeFlag = false;// 제외여부
+    private Boolean keyExcludeFlag;// 제외여부
 
     private Boolean keyBidFlag; // 수동입찰가 등록 여부
 
@@ -44,16 +24,4 @@ public class KeywordResponseDto {
     private Long bid;
 
     private Map<String,Long> keySalesOptions;
-
-    public void update(Keyword keyword){
-        keyClicks += keyword.getKeyClicks();
-        keyImpressions += keyword.getKeyImpressions();
-        keyTotalSales += keyword.getKeyTotalSales();
-        keyAdcost += keyword.getKeyAdcost();
-        keyAdsales += keyword.getKeyAdsales();
-        if(keyAdcost !=0 ) keyCpc = (double) (Math.round(((keyAdcost/ (double) keyClicks)*100)))/100.0  ;
-        if(keyAdsales !=0 ) keyRoas = (double) (Math.round((keyAdsales/keyAdcost)*10000)) / 100.0;
-        if(keyImpressions != 0 )  keyClickRate = (double) (Math.round(((double)keyClicks/(double)keyImpressions) * 10000))/100.0;
-        if(keyClicks !=0 ) keyCvr = (double) (Math.round(((double)keyTotalSales/(double)keyClicks)*10000)) /100.0;
-    }
 }

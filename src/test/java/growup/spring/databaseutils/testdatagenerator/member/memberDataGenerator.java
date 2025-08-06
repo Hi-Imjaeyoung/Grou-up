f5406a74ba4c99deb077c60df76b8c18321e4d1a
Hi-Imjaeyoung <imjy2757@naver.com>
2025-08-06 22:56:54 +0900

    refactor : 데이터 베이스 생성 테스트 코드 위치 분리

---
+++ src/test/java/growup/spring/databaseutils/testdatagenerator/member/memberDataGenerator.java
@@ -0,0 +1,40 @@
+package growup.spring.databaseutils.testdatagenerator.member;
+
+import growup.spring.databaseutils.testdatagenerator.DataFormat;
+import growup.spring.springserver.global.support.Role;
+import growup.spring.springserver.login.domain.Member;
+import growup.spring.springserver.login.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("bottleNeckTest")
+//@Disabled
+public class memberDataGenerator {
+    @Autowired
+    private MemberRepository memberRepository;
+    final Role defaultRole = Role.USER;
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {100})
+    @DisplayName("MemberGenerator")
+    void memberGenerator(int numberOfMember){
+        for(int i=1;i<=numberOfMember;i++){
+            String counter = String.valueOf(i);
+            if(i < 10) counter = "0" + i;
+            memberRepository.save(
+                    Member.builder()
+                    .name(DataFormat.USERNAME.getValue() + counter)
+                    .email(DataFormat.USERNAME.getValue()+ counter +DataFormat.EMAIL_FORMAT.getValue())
+                    .role(defaultRole)
+                    .build()
+            );
+        }
+    }
+
+}

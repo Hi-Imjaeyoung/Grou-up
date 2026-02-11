5fdab3254c170b95aeaa408be4f977aec89126f1
Hi-Imjaeyoung <imjy2757@naver.com>
2026-02-11 15:25:17 +0900

    refactor : io작업 스레드 풀 사이즈 확정.

--- src/main/java/growup/spring/springserver/global/config/ThreadPoolConfig.java
+++ src/main/java/growup/spring/springserver/global/config/ThreadPoolConfig.java
@@ -9,24 +9,24 @@ import java.util.concurrent.Executor;
 @Configuration
 public class ThreadPoolConfig {
 
-    // 1. [I/O 전용] DB 조회용 (기다리는 애들이니 넉넉하게!)
+    // 1. [I/O 전용] DB 조회용
     @Bean(name = "ioExecutor")
     public Executor ioExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(20); // DB 커넥션 풀 사이즈랑 비슷하게 맞추면 좋음
-        executor.setMaxPoolSize(20);
+        executor.setCorePoolSize(30);
+        executor.setMaxPoolSize(30);
         executor.setQueueCapacity(100);
         executor.setThreadNamePrefix("IO-Worker-");
         executor.initialize();
         return executor;
     }
 
-    // 2. [CPU 전용] 트리 빌드용 (일하는 애들이니 타이트하게!)
+    // 2. [CPU 전용] 트리 빌드용
     @Bean(name = "cpuExecutor")
     public Executor cpuExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         int processors = Runtime.getRuntime().availableProcessors();
-        executor.setCorePoolSize(processors + 1); // 코어 수 + 1
+        executor.setCorePoolSize(processors + 1);
         executor.setMaxPoolSize(processors + 1);
         executor.setQueueCapacity(500);
         executor.setThreadNamePrefix("CPU-Worker-");
